cbaf52eca6f9236c9ccf4369dac416f020b8adf1
Rahul Reghu <rahul.reghu@citrusinformatics.com>
2025-02-26 14:56:19 +0530

    hardcode fromNumber for testing purposes

--- src/app/pages/dialer/dialer.page.ts
+++ src/app/pages/dialer/dialer.page.ts
@@ -125,7 +125,7 @@ export class DialerPage implements OnInit {
     if (this.currentValue != "") {
       this.router.navigateByUrl("/dialer1", {
         state: {
-          fromNumber: this.fromNumber,
+          fromNumber: "19734595823",
           dialedPhone: this.dialedPhone,
           callerData: this.callerIdNumber,
         },
