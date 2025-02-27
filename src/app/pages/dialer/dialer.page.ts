79b98ecd0bb97b52866caeb09484d18740d28a2b
Rahul Reghu <rahul.reghu@citrusinformatics.com>
2025-02-27 11:39:19 +0530

    update fromNumber to include country code in dialer page

--- src/app/pages/dialer/dialer.page.ts
+++ src/app/pages/dialer/dialer.page.ts
@@ -125,7 +125,7 @@ export class DialerPage implements OnInit {
     if (this.currentValue != "") {
       this.router.navigateByUrl("/dialer1", {
         state: {
-          fromNumber: "19734595823",
+          fromNumber: "+19734595823",
           dialedPhone: this.dialedPhone,
           callerData: this.callerIdNumber,
         },
